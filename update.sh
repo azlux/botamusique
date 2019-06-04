60f1b8cb0a859022a3bd162ffa7994788a19cbc1
azlux <github@azlux.fr>
2019-06-04 11:01:38 +0200

    Hot Fix

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 curl -Lo /tmp/botamusique.tar.gz https://azlux.fr/botamusique/sources.tar.gz
-tar -xzf /tmp/botamusique.tar.gz -C /tmp/botamusique
+tar -xzf /tmp/botamusique.tar.gz -C /tmp/
 cp -r /tmp/botamusique/* .
 rm -r /tmp/botamusique
 rm -r /tmp/botamusique.tar.gz
