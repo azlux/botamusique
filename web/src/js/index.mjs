e0f897ea2c8769597cc74f28efd194380af8d521
Tyler Vigario <TylerVigario90@gmail.com>
2020-06-01 17:27:27 -0700

    Update web dependencies

--- web/src/js/index.mjs
+++ web/src/js/index.mjs
@@ -4,12 +4,12 @@ import { library, dom } from '@fortawesome/fontawesome-svg-core/index.es';
 import { fas } from '@fortawesome/free-solid-svg-icons/index.es';
 import { far } from '@fortawesome/free-regular-svg-icons/index.es';
 import { fab } from '@fortawesome/free-brands-svg-icons/index.es';
+library.add(fas, far, fab);
 
 document.addEventListener('DOMContentLoaded', () => {
     Theme.init();
 
     // FontAwesome
-    library.add(fas, far, fab);
     dom.i2svg();
 
     document.getElementById('theme-switch-btn').addEventListener('click', () => {
