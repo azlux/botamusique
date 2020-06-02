6414507fbe07d4bacbc633660adca501bc6a80ff
Tyler Vigario <TylerVigario90@gmail.com>
2020-06-01 22:15:13 -0700

    Remove FontAwesome Brands icon set

--- web/src/js/app.mjs
+++ web/src/js/app.mjs
@@ -13,14 +13,14 @@ import Theme from './theme.mjs';
 import {library, dom} from '@fortawesome/fontawesome-svg-core/index.es';
 import {fas} from '@fortawesome/free-solid-svg-icons/index.es';
 import {far} from '@fortawesome/free-regular-svg-icons/index.es';
-import {fab} from '@fortawesome/free-brands-svg-icons/index.es';
-library.add(fas, far, fab);
+library.add(fas, far);
 
 document.addEventListener('DOMContentLoaded', () => {
   Theme.init();
 
-  // This is required to seach DOM and convert i tags to SVG
-  dom.i2svg();
+  // Replace any existing <i> tags with <svg> and set up a MutationObserver to
+  // continue doing this as the DOM changes.
+  dom.watch();
 
   document.getElementById('theme-switch-btn').addEventListener('click', () => {
     Theme.swap();
