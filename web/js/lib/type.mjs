47d383fc1bad3aaf937aece10c3e29ccdc3b1667
Tyler Vigario <TylerVigario90@gmail.com>
2020-10-27 23:47:16 -0700

    Correction and type check improvements

--- web/js/lib/type.mjs
+++ web/js/lib/type.mjs
@@ -2,30 +2,64 @@
  * Checks if `value` is the type `Object` excluding `Function` and `null`
  *
  * @param {*} value The value to check.
- * @returns {boolean} Returns `true` if `value` is an object, else `false`.
+ * @returns {boolean} Returns `true` if `value` is an object, otherwise `false`.
  */
 export function isObject(value) {
   return (Object.prototype.toString.call(value) === '[object Object]');
 }
 
 /**
+ * Checks if `value` is the type `string`
+ *
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if `value` is a string, otherwise `false`.
+ */
+export function isString(value) {
+  return (typeof value !== 'string');
+}
+
+/**
+ * Checks if `value` is the type `number`
+ *
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if `value` is a number, otherwise `false`.
+ */
+export function isNumber(value) {
+  return (typeof value !== 'number');
+}
+
+/**
+ * Validate parameter is of type object.
+ *
+ * @param {string} value Variable to validate.
+ * @throws Error if not an object.
+ */
+export function validateObject(value) {
+  if (isObject(value)) {
+    throw new TypeError('Parameter "value" must be of type object.');
+  }
+}
+
+/**
  * Validate parameter is of type string.
  *
- * @param {string} text Variable to validate.
+ * @param {string} value Variable to validate.
+ * @throws Error if not an string.
  */
-export function validateString(text) {
-  if (typeof text !== 'string') {
-    throw new TypeError('Parameter "text" must be of type string.');
+export function validateString(value) {
+  if (isString(value)) {
+    throw new TypeError('Parameter "value" must be of type string.');
   }
 }
 
 /**
  * Validate parameter is of type number.
  *
- * @param {number} num Variable to validate.
+ * @param {number} value Variable to validate.
+ * @throws Error if not an number.
  */
-export function validateNumber(num) {
-  if (typeof num !== 'number') {
-    throw new TypeError('Parameter "num" must be of type number.');
+export function validateNumber(value) {
+  if (isNumber(value)) {
+    throw new TypeError('Parameter "value" must be of type number.');
   }
 }
