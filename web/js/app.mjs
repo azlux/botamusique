ede65b3afbe936b85d7036d198520206fac442e1
Tyler Vigario <TylerVigario90@gmail.com>
2020-11-24 18:24:20 -0800

    Only include FA icons in-use

--- web/js/app.mjs
+++ web/js/app.mjs
@@ -1,7 +1,17 @@
 import {library, dom} from '@fortawesome/fontawesome-svg-core/index.es.js';
-import {fas} from '@fortawesome/free-solid-svg-icons/index.es.js';
-import {far} from '@fortawesome/free-regular-svg-icons/index.es.js';
-library.add(fas, far);
+import {
+  faTimesCircle, faPlus, faCheck, faUpload, faTimes, faTrash, faPlay, faPause, faFastForward, faPlayCircle, faLightbulb,
+  faTrashAlt, faDownload, faSyncAlt, faEdit, faVolumeUp, faVolumeDown, faRobot, faRedo, faRandom, faTasks
+} from '@fortawesome/free-solid-svg-icons/index.es.js';
+import {faFileAlt} from '@fortawesome/free-regular-svg-icons/index.es.js';
+
+library.add(
+  // Solid
+  faTimesCircle, faPlus, faCheck, faUpload, faTimes, faTrash, faPlay, faPause, faFastForward, faPlayCircle, faLightbulb,
+  faTrashAlt, faDownload, faSyncAlt, faEdit, faVolumeUp, faVolumeDown, faRobot, faRedo, faRandom, faTasks,
+  // Regular
+  faFileAlt
+);
 
 // Old application code
 import './main.mjs';
