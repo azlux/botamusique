17a35c43e30e00d0385732764afe546ff1eeb146
SeerLite <seerlite@tuta.io>
2021-03-13 02:39:46 -0300

    Avoid auto pausing when playlist is empty

--- mumbleBot.py
+++ mumbleBot.py
@@ -356,8 +356,8 @@ class MumbleBot:
             elif var.config.get("bot", "when_nobody_in_channel") == "pause":
                 self.send_channel_msg(tr("auto_paused"))
 
-        elif len(own_channel.get_users()) == 1:
-            # if the bot is the only user left in the channel
+        elif len(own_channel.get_users()) == 1 and len(var.playlist) != 0:
+            # if the bot is the only user left in the channel and the playlist isn't empty
             self.log.info('bot: Other users in the channel left. Stopping music now.')
 
             if var.config.get("bot", "when_nobody_in_channel") == "stop":
