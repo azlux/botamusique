849a0a08430289acecc9f9ab071128342bd47952
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-05-13 17:58:34 +0200

    Only fetch changelog, when auto-updates are enabled
    Fixes: #268

--- mumbleBot.py
+++ mumbleBot.py
@@ -190,8 +190,9 @@ class MumbleBot:
         last_startup_version = var.db.get("bot", "version", fallback=None)
         if not last_startup_version or version.parse(last_startup_version) < version.parse(self.version):
             var.db.set("bot", "version", self.version)
-            changelog = util.fetch_changelog()
-            self.send_channel_msg(tr("update_successful", version=self.version, changelog=changelog))
+            if var.config.getboolean("bot", "auto_check_update"):
+                changelog = util.fetch_changelog()
+                self.send_channel_msg(tr("update_successful", version=self.version, changelog=changelog))
 
     # Set the CTRL+C shortcut
     def ctrl_caught(self, signal, frame):
